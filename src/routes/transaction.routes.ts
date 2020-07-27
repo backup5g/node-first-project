61241891b7c7c3f5f522e726c2cd3949f800e49d
Diego Eiras <eiras.lucio@gmail.com>
2020-07-27 10:25:13 -0300

    ⏩ Initial commit

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,26 +1,43 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
-const transactionRouter = Router();
+const router = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const repository = new TransactionsRepository();
 
-transactionRouter.get('/', (request, response) => {
+router.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = repository.all();
+
+    const balance = repository.getBalance();
+
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
-transactionRouter.post('/', (request, response) => {
+router.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const service = new CreateTransactionService(repository);
+
+    const createTransaction = service.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(createTransaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
-export default transactionRouter;
+export default router;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,15 +1,38 @@
+import { response } from 'express';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+  private repository: TransactionsRepository;
 
   constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
+    this.repository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('The type of transaction must be income or outcome');
+    }
+
+    const { total } = this.repository.getBalance();
+
+    if (type === 'outcome' && total - value < 0) {
+      throw Error('You cannot make transactions without money');
+    }
+
+    const transaction = this.repository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
